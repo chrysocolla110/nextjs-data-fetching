feb832bdaf5455037e1aea20e8ffb3298de846c0
chrysocolla110 <101643060+chrysocolla110@users.noreply.github.com>
2023-04-26 20:40:43 -0700

    1 file modified

--- pages/index.js
+++ pages/index.js
@@ -20,24 +20,36 @@ export default function Home() {
 
         <div className={styles.grid}>
           <a href="./client-side" className={styles.card}>
-            <h3>Client-Side &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
+            <h3>CSR - Client-Side Rendering &rarr;</h3>
+            <p>
+              A common data fetching method using useEffect. It will fetch the
+              data from the API every single page request on the client-side
+              (after the page is rendered, then the function will run).
+            </p>
           </a>
 
           <a href="./server-side" className={styles.card}>
-            <h3>Server-Side &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
+            <h3>SSR - Server-Side Rendering &rarr;</h3>
+            <p>
+              A special function to fetch data from the API with every page
+              request on the server-side (before the page is loaded, that
+              special function will run first, creating a delay, then after
+              that, it will serve the page).
+            </p>
           </a>
 
           <a href="./static-site" className={styles.card}>
-            <h3>Static-Site &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
+            <h3>SSG - Static Site &rarr;</h3>
+            <p>A special function to fetch data once when that page builds.</p>
           </a>
 
           <a href="./incremental-static" className={styles.card}>
-            <h3>Incremental-Static &rarr;</h3>
+            <h3>ISR – Incremental Static Regeneration &rarr;</h3>
             <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
+              A new method. Shortly put, it is a combination of SSG, and SSR,
+              where it served statically, but at a certain time and certain
+              condition, that page will rebuild and fetch the data from the API
+              again.
             </p>
           </a>
         </div>
